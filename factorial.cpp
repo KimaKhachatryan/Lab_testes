f2dcd9153cf8b4b56efbfee10c0335a83b1390ca
Kima <kimakhachatryan@mail.ru>
2023-11-15 16:26:19 +0400

    first comit
    Added files

---
+++ factorial.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+long long factorial(int num)
+{
+	if (num < 0) {
+		return -1;
+	} else if (num == 0 || num == 1) {
+		return num;
+	}
+
+	int res = 1;
+
+	while (num) {
+		res *= num;
+		--num;
+	}
+
+	return res;
+}
+
+int main()
+{
+	int num = 0;
+	
+	std::cin >> num;
+	std::cout << factorial(num) << std::endl;
+	return 0;
+}
+
